f0c68657f19769c21eb65fdce7af899cc92c81e2
chlwlgus4 <chlwlgus89@naver.com>
2021-03-21 17:22:46 +0900

    h2 데이터베이스 연결

--- src/main/java/com/hello/hellospring/SpringConfig.java
+++ src/main/java/com/hello/hellospring/SpringConfig.java
@@ -1,14 +1,24 @@
 package com.hello.hellospring;
 
+import com.hello.hellospring.repository.JdbcMemberRepository;
 import com.hello.hellospring.repository.MemberRepository;
-import com.hello.hellospring.repository.MemoryMemberRepository;
 import com.hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 //스프링에서 빈을 찾을 때 빈을 주입해줌
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     //스프링 컨테이너에 빈을 올림
     @Bean
@@ -19,6 +29,7 @@ public class SpringConfig {
     //스프링 컨테이너에 빈을 올림
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
